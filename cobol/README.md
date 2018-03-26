5bcb62046617f9e6325da13b1f31f16b35d9d5a5
wtee <wtee@users.noreply.github.com>
2018-03-25 23:29:08 -0500

    Add COBOL README.md and reorganize COBOL folder

---
+++ cobol/README.md
@@ -0,0 +1,5 @@
+# COBOL
+
+Programs within this directory should successfully compile with
+[GnuCOBOL](https://sourceforge.net/projects/open-cobol/) using the
+'--free' flag. Ex. `cobc --free -x hello.cbl`
---
+++ cobol/hello_world/hello.cbl
@@ -0,0 +1,11 @@
+identification division.
+program-id. hello.
+
+data division.
+working-storage section.
+01 name     pic x(12) value "Grace Hopper".
+01 newline  pic x(1)  value x'0a'. 
+
+procedure division.
+display "Hello, " name "! " newline "Thanks for making COBOL possible.".
+stop run.
