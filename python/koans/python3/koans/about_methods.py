bc043b4b4b2870da01136abe127e104f307848db
wtee <wtee@users.noreply.github.com>
2018-04-18 18:47:18 -0500

    Completed some more koans

--- python/koans/python3/koans/about_methods.py
+++ python/koans/python3/koans/about_methods.py
@@ -12,7 +12,7 @@ def my_global_function(a,b):
 
 class AboutMethods(Koan):
     def test_calling_a_global_function(self):
-        self.assertEqual(__, my_global_function(2,3))
+        self.assertEqual(5, my_global_function(2,3))
 
     # NOTE: Wrong number of arguments is not a SYNTAX error, but a
     # runtime error.
