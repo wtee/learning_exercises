f87a2f8e391ac288f5c891ee029f68bb6ef9a371
wtee <wtee@users.noreply.github.com>
2019-03-17 10:01:14 -0500

    Add common_lisp/exercism/hamming.lisp

---
+++ common_lisp/exercism/hamming.lisp
@@ -0,0 +1,21 @@
+(defpackage #:hamming
+  (:use #:cl)
+  (:export #:distance))
+
+(in-package #:hamming)
+
+(defun distance (dna1 dna2)
+  "Calculate the number of positional differences, or Hamming distance,
+   in two equal length dna strands."
+  ;; If the strings are not the same length, return nil.
+  (if (not (= (length dna1) (length dna2)))
+      nil
+      ;; If the strings are empty return 0.
+      (cond ((eql 0 (length dna1)) 0)
+            ;; If the two characters are the same, call distance on the
+            ;; remaining string.
+            ((eql (char dna1 0) (char dna2 0))
+             (distance (subseq dna1 1) (subseq dna2 1)))
+            ;; Otherwise, the characters are different, so return 1 plus
+            ;; the result of calling distance on the remainder of the list.
+            (t (+ 1 (distance (subseq dna1 1) (subseq dna2 1)))))))
