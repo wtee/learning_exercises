c88b2460a1d7c2c1715703c491f57761abdd07eb
wtee <wtee@users.noreply.github.com>
2019-04-06 14:46:21 -0500

    Tweak equality tests in hamming.lisp

--- common_lisp/exercism/hamming.lisp
+++ common_lisp/exercism/hamming.lisp
@@ -8,8 +8,8 @@
   "Calculate the number of positional differences, or Hamming distance,
    in two equal length dna strands."
   (cond
-    ((not (eql (length dna1) (length dna2))) nil)
-    ((eql 0 (length dna1)) 0)
+    ((not (= (length dna1) (length dna2))) nil)
+    ((= 0 (length dna1)) 0)
     ((char= (char dna1 0) (char dna2 0))
       (distance (subseq dna1 1) (subseq dna2 1)))
     (t (+ 1 (distance (subseq dna1 1) (subseq dna2 1))))))
