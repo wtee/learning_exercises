d3e297fea5b1d94adff6cc005749fa276ecf0157
wtee <wtee@users.noreply.github.com>
2019-04-07 11:13:18 -0500

    Add C# exercises

---
+++ csharp/exercism/Pangram.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        HashSet<char> uniqueLetters = new HashSet<char>();
+
+        foreach (char c in input.ToLower())
+        {
+            if (Pangram.IsAlpha(c))
+            {
+                uniqueLetters.Add(c);
+            }
+        }
+        
+        if (uniqueLetters.Count == 26)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static bool IsAlpha(char input)
+    {
+        Regex rx = new Regex("[A-Za-z]");
+        
+        if (rx.IsMatch(input.ToString()))
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
+
---
+++ csharp/exercism/TwoFer.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public static class TwoFer
+{
+    public static string Speak(string name = "you")
+    {
+        return System.String.Format("One for {0}, one for me.", name);
+    }
+}
