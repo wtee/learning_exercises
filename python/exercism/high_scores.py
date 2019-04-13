faebafceab416ee2720bf6ad918f26e83aa2caea
wtee <wtee@users.noreply.github.com>
2019-04-13 10:19:12 -0500

    Add high_scores.py

---
+++ python/exercism/high_scores.py
@@ -0,0 +1,14 @@
+class HighScores(object):
+    def __init__(self, scores):
+        self.scores = scores
+        self.sorted_scores = sorted(scores, reverse=True)
+
+    def latest(self):
+        return self.scores[-1]
+
+    def personal_best(self):
+        return self.sorted_scores[0]
+
+    def personal_top_three(self):
+        return self.sorted_scores[0:3]
+
