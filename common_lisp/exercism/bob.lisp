85853df309f63906be92390cf4b65466a89ea666
wtee <wtee@users.noreply.github.com>
2019-04-14 22:49:12 -0500

    Add bob.lisp

---
+++ common_lisp/exercism/bob.lisp
@@ -0,0 +1,26 @@
+(in-package #:cl-user)
+(defpackage #:bob
+  (:use #:cl)
+  (:export #:response-for))
+(in-package #:bob)
+
+(defun response-for (input)
+  (let ((clean-input (trim-whitespace input)))
+    (cond
+      ((= 0 (length clean-input)) "Fine. Be that way!")
+      ((and (all-caps-p clean-input) (last-char= clean-input #\?))
+       "Calm down, I know what I'm doing!")
+      ((all-caps-p clean-input) "Whoa, chill out!")
+      ((last-char= clean-input #\?) "Sure.")
+      (t "Whatever."))))
+
+(defun all-caps-p (input)
+  (and (string= input (string-upcase input))
+       (not (string= input (string-downcase input)))))
+
+(defun last-char= (input test-char)
+  (char= (char input (- (length input) 1)) test-char))
+
+(defun trim-whitespace (input)
+  (let ((whitespace '(#\Backspace #\Linefeed #\Newline #\Page #\Return #\Rubout #\Space #\Tab)))
+    (string-trim whitespace input))) 
