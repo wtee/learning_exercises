4c63b875f8d5ad22221b6ad0503eecb467e0cda2
wtee <wtee@users.noreply.github.com>
2019-06-10 07:08:57 -0500

    Add matrix.py

---
+++ python/exercism/matrix.py
@@ -0,0 +1,14 @@
+class Matrix(object):
+    def __init__(self, matrix_string):
+        self.matrix = [ 
+                        [ int(point)
+                          for point
+                          in row.split(" ") ]
+                        for row
+                        in matrix_string.split("\n") ]
+
+    def row(self, index):
+        return self.matrix[index - 1]
+
+    def column(self, index):
+        return [ row[index - 1] for row in self.matrix]
