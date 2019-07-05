01cfad681797c782085be1660b410e0da72d53dd
wtee <wtee@users.noreply.github.com>
2019-07-05 11:53:23 -0500

    Reformat matrix.py

--- python/exercism/matrix.py
+++ python/exercism/matrix.py
@@ -1,14 +1,12 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        self.matrix = [ 
-                        [ int(point)
-                          for point
-                          in row.split(" ") ]
-                        for row
-                        in matrix_string.split("\n") ]
+        self.matrix = [
+            [int(point) for point in row.split(" ")]
+            for row in matrix_string.split("\n")
+        ]
 
     def row(self, index):
         return self.matrix[index - 1]
 
     def column(self, index):
-        return [ row[index - 1] for row in self.matrix]
+        return [row[index - 1] for row in self.matrix]
