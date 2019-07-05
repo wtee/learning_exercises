c4c0fe0a27e6a08f49e7f05e8b50309162988625
wtee <wtee@users.noreply.github.com>
2019-07-05 11:42:50 -0500

    Refactor and simplify dna.lisp

--- common_lisp/exercism/dna.lisp
+++ common_lisp/exercism/dna.lisp
@@ -4,31 +4,20 @@
   (:export #:to-rna))
 (in-package #:dna)
 
-(defun only-one-nucleotide-p (str)
-  (= 1 (length str)))
-
 (defun is-valid-dna-p (str)
   (search str "GCTA"))
 
 (defun transcribe (str)
-  (let ((dna "GCTA")
+    (if (is-valid-dna-p str)
+      (let ((dna "GCTA")
         (rna "CGAU"))
-    (string (char rna (search str dna)))))
+        (string (char rna (search str dna))))
+      (error "~S is not a valid nucleotide." str)))
 
-(defun one-to-rna (str)
-  (if (is-valid-dna-p str)
-    (transcribe str)
-    (error "~S is not a valid nucleotide." str)))
-
-(defun many-to-rna (str)
+(defun to-rna (str)
+  "Transcribe a string representing DNA nucleotides to RNA."
   (format nil "~{~a~}"
     (loop
     for n
     across str
-    collect (one-to-rna (string n)))))
-
-(defun to-rna (str)
-  "Transcribe a string representing DNA nucleotides to RNA."
-    (if (only-one-nucleotide-p str)
-      (one-to-rna str)
-      (many-to-rna str)))
+    collect (transcribe (string n)))))
