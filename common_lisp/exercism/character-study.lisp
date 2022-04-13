2424460233596294645aea623ddee929f8d031c9
wtee <wtee@users.noreply.github.com>
2022-04-12 20:03:12 -0500

    Add character-study.lisp

---
+++ common_lisp/exercism/character-study.lisp
@@ -0,0 +1,33 @@
+(defpackage :character-study
+  (:use :cl)
+  (:export
+   :compare-chars
+   :size-of-char
+   :change-size-of-char
+   :type-of-char))
+(in-package :character-study)
+
+(defun compare-chars (char1 char2)
+  (cond
+    ((char< char1 char2) :less-than)
+    ((char= char1 char2) :equal-to)
+    ((char> char1 char2) :greater-than)))
+
+(defun size-of-char (char)
+  (cond
+    ((upper-case-p char) :big)
+    ((lower-case-p char) :small)
+    (t :no-size)))
+
+(defun change-size-of-char (char wanted-size)
+  (case wanted-size
+    (:big (char-upcase char))
+    (:small (char-downcase char))))
+
+(defun type-of-char (char)
+  (cond
+    ((alpha-char-p char) :alpha)
+    ((digit-char-p char) :numeric)
+    ((char= char #\space) :space)
+    ((char= char #\newline) :newline)
+    (t :unknown)))
