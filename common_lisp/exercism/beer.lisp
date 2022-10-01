46eaa5ad909f71fce5725a714ef6689a318f0b18
wtee <wtee@users.noreply.github.com>
2022-10-01 10:24:12 -0500

    Refactor beer.lisp again

--- common_lisp/exercism/beer.lisp
+++ common_lisp/exercism/beer.lisp
@@ -6,17 +6,20 @@
 
 (defun verse (n)
   "Returns a string verse for a given number."
-  (format nil
-          "~:[~a~;No more~] bottle~p of beer on the wall, ~:[~a~;no more~] bottle~p of beer.~%~:[Take ~:[one~;it~] down and pass it around, ~:[~a~;no more~]~;Go to the store and buy some more, 99~] bottle~p of beer on the wall.~%" 
-          (zerop n) n
-          n
-          (zerop n) n
-          n
-          (zerop n)
-          (= n 1)
-          (= n 1)
-          (- n 1)
-          (- n 1)))
+  (let ((n-is-zero? (zerop n))
+        (n-equals-one? (= n 1))
+        (n-minus-one (- n 1)))
+    (format nil
+            "~:[~a~;No more~] bottle~p of beer on the wall, ~:[~a~;no more~] bottle~p of beer.~%~:[Take ~:[one~;it~] down and pass it around, ~:[~a~;no more~]~;Go to the store and buy some more, 99~] bottle~p of beer on the wall.~%" 
+            n-is-zero? n
+            n
+            n-is-zero? n
+            n
+            n-is-zero?
+            n-equals-one?
+            n-equals-one?
+            n-minus-one
+            n-minus-one)))
 
 (defun sing (start &optional (end 0))
   "Returns a string of verses for a given range of numbers."
