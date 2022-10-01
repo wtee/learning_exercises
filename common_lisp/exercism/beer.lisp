1264dc1e5727acac7517f11c8a940511c1003c6d
wtee <wtee@users.noreply.github.com>
2022-10-01 10:00:15 -0500

    Refactor beer.lisp

--- common_lisp/exercism/beer.lisp
+++ common_lisp/exercism/beer.lisp
@@ -6,20 +6,18 @@
 
 (defun verse (n)
   "Returns a string verse for a given number."
-  (let ((n-formatted (if (zerop n) "No more" (write-to-string n)))
-        (es1 (if (= n 1) "" "s"))
-        (do-what 
-          (cond
-            ((> n 1) "Take one down and pass it around")
-            ((= n 1) "Take it down and pass it around")
-            ((zerop n) "Go to the store and buy some more")))
-        (new-n (cond 
-                 ((= n 1) "no more")
-                 ((zerop n) 99)
-                 (t (- n 1))))
-        (es2 (if (= n 2) "" "s")))
-  (format nil "~a bottle~a of beer on the wall, ~a bottle~a of beer.~%~a, ~a bottle~a of beer on the wall.~%" n-formatted es1 (string-downcase n-formatted) es1 do-what new-n es2)))
-     
+  (format nil
+          "~:[~a~;No more~] bottle~p of beer on the wall, ~:[~a~;no more~] bottle~p of beer.~%~:[Take ~:[one~;it~] down and pass it around, ~:[~a~;no more~]~;Go to the store and buy some more, 99~] bottle~p of beer on the wall.~%" 
+          (zerop n) n
+          n
+          (zerop n) n
+          n
+          (zerop n)
+          (= n 1)
+          (= n 1)
+          (- n 1)
+          (- n 1)))
+
 (defun sing (start &optional (end 0))
   "Returns a string of verses for a given range of numbers."
   (format nil "~{~a~%~}" (loop for n from start downto end by 1 collect (verse n))))
