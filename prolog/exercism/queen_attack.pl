f9cc04aa30105a4297613cf1c2e67a1e68c1f764
wtee <wtee@users.noreply.github.com>
2022-10-01 10:34:41 -0500

    Fix create() comparisons

--- prolog/exercism/queen_attack.pl
+++ prolog/exercism/queen_attack.pl
@@ -1,15 +1,7 @@
-% Please visit https://exercism.io/tracks/prolog/installation
-% for instructions on setting up prolog.
-% Visit https://exercism.io/tracks/prolog/tests
-% for help running the tests for prolog exercises.
-
-% Replace the goal below with
-% your implementation.
-
 same_row_or_column((A, B), (X, Y)) :- (A = X); (B = Y).
 is_diagonal((A, B), (X, Y)) :- A + B =:= X + Y; A + X =:= B + Y.
 
-create((X, Y)) :- X = Y, X @=< 8, X @>= 1, Y @=< 8, Y @>= 1.
+create((X, Y)) :- X = Y, X =< 8, X >= 1, Y =< 8, Y >= 1.
 
 attack((A, B), (X, Y)) :- same_row_or_column((A, B), (X, Y));
                           is_diagonal((A, B), (X, Y)).
