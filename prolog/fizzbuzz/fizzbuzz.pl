f16b022f80d890009f6443cbb436e56214c98601
wtee <wtee@users.noreply.github.com>
2022-10-02 20:43:01 -0500

    Fix weird formatting

--- prolog/fizzbuzz/fizzbuzz.pl
+++ prolog/fizzbuzz/fizzbuzz.pl
@@ -1,15 +1,15 @@
 divisible_by_3(N) :- 0 is mod(N, 3).
+
 divisible_by_5(N) :- 0 is mod(N, 5).
-not_divisible_by_3_or_5(N) :- \+ (divisible_by_3(N)
-                                 ; divisible_by_5(N)
-                                 ).
+
+not_divisible_by_3_or_5(N) :-
+  \+ (divisible_by_3(N); divisible_by_5(N)).
 
 fizzbuzz_atom(N) :-
-    (not_divisible_by_3_or_5(N)
-    -> write(N)
-    ; (divisible_by_3(N) -> write('fizz'); true),
-      (divisible_by_5(N) -> write('buzz'); true)
-    ), nl.
+    (not_divisible_by_3_or_5(N) -> write(N);
+    (divisible_by_3(N) -> write('fizz'); true),
+      (divisible_by_5(N) -> write('buzz'); true)),
+    nl.
 
 fizzbuzz_list([]).
 fizzbuzz_list([H|T]) :- fizzbuzz_atom(H), fizzbuzz_list(T).
